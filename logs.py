9d40cd3b9f4b129fbd036b24fe80dddb14e841a9
HenriqueMonteiro35 <43910254+HenriqueMonteiro35@users.noreply.github.com>
2025-10-12 04:13:53 -0400

    Create logs.py

---
+++ logs.py
@@ -0,0 +1,15 @@
+# Decorator
+def record_log(func):
+    def wrapper(self, *args, **kwargs):
+        logs = self._logs + [(func.__name__, (*args, kwargs))]
+        return func(self, *args, logs=logs, **kwargs)
+    return wrapper
+
+def format_log(log_entry):
+    fun, args = log_entry
+    args_string = " & ".join(map(lambda x: x.__str__(), args))
+    if fun == "filter":
+        args_string = args_string.replace("[", "").replace("]", "").strip()
+
+    args_string = args_string.replace("& {}", "")
+    return f"==> {fun.upper()}: {args_string}"
