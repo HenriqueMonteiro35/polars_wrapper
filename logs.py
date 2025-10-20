9c1d7c416f7dd85738330c80c32a489d6729f0ac
HenriqueMonteiro35 <43910254+HenriqueMonteiro35@users.noreply.github.com>
2025-10-20 01:12:56 -0400

    Update logs.py

--- logs.py
+++ logs.py
@@ -1,13 +1,16 @@
+# logs.py
+
 # Decorator
-def record_log(func):
-    def wrapper(self, *args, **kwargs):
-        logs = self._logs + [(func.__name__, (*args, kwargs))]
-        return func(self, *args, logs=logs, **kwargs)
-    return wrapper
+# def update_log(func):
+#     def wrapper(self, *args, **kwargs):
+#         logs = self._logs + [(func.__name__, (*args, kwargs))]
+#         return func(self, *args, logs=logs, **kwargs)
+#     return wrapper
 
 def format_log(log_entry):
     fun, args = log_entry
     args_string = " & ".join(map(lambda x: x.__str__(), args))
+
     if fun == "filter":
         args_string = args_string.replace("[", "").replace("]", "").strip()
 
